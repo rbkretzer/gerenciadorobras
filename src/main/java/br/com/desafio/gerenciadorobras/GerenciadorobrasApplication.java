2f5c39cef7f9ae6f9f423bc9a3dc39729c8835c6
rbkretzer <rafael.bkretzer@gmail.com>
2023-02-27 18:34:57 -0300

    início do projeto para gerenciamento de obras

---
+++ src/main/java/br/com/desafio/gerenciadorobras/GerenciadorobrasApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.gerenciadorobras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorobrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorobrasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafio/gerenciadorobras/GerenciadorobrasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.gerenciadorobras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorobrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
